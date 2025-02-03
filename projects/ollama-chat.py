634e8f46c399e88cda03f3784589ecda2fdcdd33
anoop <learn4tarakki@gmail.com>
2025-02-03 22:40:57 +0530

    updated prompt

--- projects/ollama-chat.py
+++ projects/ollama-chat.py
@@ -2,7 +2,7 @@ from langchain_ollama import ChatOllama
 
 model = ChatOllama(model="deepseek-r1:1.5b", base_url="http://localhost:11434")
 
-messages = [("system","you are a scientist, respond in 10 words"), ("human", "how can i make rocket.")]
+messages = [("system","you are a scientist, respond in short"), ("human", "how can i make rocket.")]
 
 response = model.invoke(messages)
 
